5c40c2694c03c8b9aa7e6d25b83b7a7e1694a9dd
Vaishali Kumanan <vaishali.kumanan@gmail.com>
2020-06-25 20:51:31 -0400

    created map from english to tamil characters

---
+++ src/tamil.js
@@ -0,0 +1,150 @@
+const table = {
+    "a": {
+        "unicode": "\u0b85",
+        "concat": "\u200b"
+    },
+    "aa": {
+        "unicode": "\u0b86",
+        "concat": "\u0bbe"
+    },
+    "i": {
+        "unicode": "\u0b87",
+        "concat": "\u0bbf"
+    },
+    "ii": {
+        "unicode": "\u0b88",
+        "concat": "\u0bc0"
+    },
+    "u": {
+        "unicode": "\u0b89",
+        "concat": "\u0bc1"
+    },
+    "uu": {
+        "unicode": "\u0b8a",
+        "concat": "\u0bc2"
+    },
+    "e": {
+        "unicode": "\u0b8e",
+        "concat": "\u0bc6"
+    },
+    "ee": {
+        "unicode": "\u0b8f",
+        "concat": "\u0bc7"
+    },
+    "ai": {
+        "unicode": "\u0b90",
+        "concat": "\u0bc8"
+    },
+    "o": {
+        "unicode": "\u0b92",
+        "concat": "\u0bca"
+    },
+    "oo": {
+        "unicode": "\u0b93",
+        "concat": "\u0bcb"
+    },
+    "au": {
+        "unicode": "\u0b94",
+        "concat": "\u0bcc"
+    },
+    ".": {
+        "unicode": "\u0b83",
+        "concat": "\u0bcd"
+    },
+    "k": {
+        "unicode": "\u0b95", 
+        "concat": "" 
+    },
+    "g": {
+        "unicode": "\u0b95", 
+        "concat": "" 
+    },
+    "ng": {
+        "unicode": "\u0b99", 
+        "concat": "" 
+    },
+    "s": {
+        "unicode": "\u0b9a", 
+        "concat": "" 
+    },
+    "j": {
+        "unicode": "\u0b9c", 
+        "concat": "" 
+    },
+    "ny": {
+        "unicode": "\u0b9e", 
+        "concat": "" 
+    },
+    "d": {
+        "unicode": "\u0b9f", 
+        "concat": "" 
+    },
+    "t": {
+        "unicode": "\u0b9f", 
+        "concat": "" 
+    },
+    "N": {
+        "unicode": "\u0ba3", 
+        "concat": "" 
+    },
+    "th": {
+        "unicode": "\u0ba4", 
+        "concat": "" 
+    },
+    "-n": {
+        "unicode": "\u0ba8", 
+        "concat": "" 
+    },
+    "n": {
+        "unicode": "\u0ba9", 
+        "concat": "" 
+    },
+    "p": {
+        "unicode": "\u0baa", 
+        "concat": "" 
+    },
+    "m": {
+        "unicode": "\u0bae", 
+        "concat": "" 
+    },
+    "y": {
+        "unicode": "\u0baf", 
+        "concat": "" 
+    },
+    "r": {
+        "unicode": "\u0bb0", 
+        "concat": "" 
+    },
+    "R": {
+        "unicode": "\u0bb1", 
+        "concat": "" 
+    },
+    "l": {
+        "unicode": "\u0bb2", 
+        "concat": "" 
+    },
+    "L": {
+        "unicode": "\u0bb3", 
+        "concat": "" 
+    },
+    "zh": {
+        "unicode": "\u0bb4", 
+        "concat": "" 
+    },
+    "v": {
+        "unicode": "\u0bb5", 
+        "concat": "" 
+    },
+    "sh": {
+        "unicode": "\u0bb7", 
+        "concat": "" 
+    },
+    "S": {
+        "unicode": "\u0bb8", 
+        "concat": "" 
+    },
+    "h": {
+        "unicode": "\u0bb9", 
+        "concat": "" 
+    }
+}
